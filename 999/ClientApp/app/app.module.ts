b04015295f4b0f6e0ba4583249c5a445ef99c726
liceist2008@mail.ru <liceist2008@mail.ru>
2019-10-25 18:31:16 +0600

    Добавьте файлы проекта.

---
+++ 999/ClientApp/app/app.module.ts
@@ -0,0 +1,18 @@
+﻿import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+
+const appRoutes: Routes = [
+	{ path: '', component: HomePageComponent },
+	
+];
+@NgModule({
+	imports: [BrowserModule, FormsModule, HttpClientModule, RouterModule.forRoot(appRoutes)],
+	declarations: [AppComponent, HomePageComponent],
+	bootstrap: [AppComponent]
+})
+export class AppModule { }
